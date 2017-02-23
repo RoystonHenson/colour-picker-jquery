740ab0dba0e7e0fbe9015cabd861fd801b292f15
Royston Henson <rlqhenson@hotmail.com>
2017-02-23 14:47:51 +0000

    add ready function with colours showing on page load

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ function addBox(color) {
   $('#colors').prepend('<div class=\'item\' style=\'background-color: ' + color + ';\'></div>');
 }
 
+$(document).ready(function() {
+  var colors = ['rgb(255, 0, 0)', 'rgb(199, 21, 133)', 'rgb(249, 192, 255)',
+                'rgb(255, 255, 0)', 'rgb(0, 0, 255)', 'rgb(0, 128, 0)',
+                'rgb(255, 165, 0)', 'rgb(255, 204, 0)', 'rgb(13, 152, 186)',
+                'rgb(255, 69 ,0)', 'rgb(128,128,128)', 'rgb(138,43,226)',
+                'rgb(205,133,63)', 'rgb(255,105,180)', 'rgb(75,0,130)',
+                'rgb(127,255,212)'];
+  colors.forEach(function(color) {
+    addBox(color);
+  });
+});
+
 $(document).on('keyup', '#color', function() {
   setPreviewColor($('#color').val());
 });
