dd07ab78ec855553f0110daba45f98a2777d6712
Royston Henson <rlqhenson@hotmail.com>
2017-02-23 14:19:03 +0000

    add function and event handler to display favourite colour boxes

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ function setPreviewColor(color) {
   $('#color-code').text($('#preview').css('background-color'));
 }
 
+function addBox(color) {
+  $('#colors').prepend('<div class=\'item\' style=\'background-color: ' + color + ';\'></div>');
+}
+
 $(document).on('keyup', '#color', function() {
   setPreviewColor($('#color').val());
+});
+
+$(document).on('click', '#add-to-favorite', function() {
+  addBox($('#color').val());
 });
